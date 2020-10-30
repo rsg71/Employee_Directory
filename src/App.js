1a937e3d47366537c91bfc3581c0e6593e158967
Robert Greenawalt <robert.greenawalt29@gmail.com>
2020-10-30 00:34:04 -0400

    added more test employees to employees.json; searchTerm functionality is being built in order to filter employees

--- src/App.js
+++ src/App.js
@@ -6,20 +6,43 @@ import employees from "./employees.json"
 
 
 
+//practice using filter on an array. Why isn't this working for the employees.json? Answer: because you had item.includes instead of item.name.includes
+
+
 class App extends React.Component {
   // Setting the component's initial state
   state = {
-    firstName: "",
+    search: "",
     employees: employees,
     sortOrder: "ASC"
   };
+
 
   handleInputChange = event => {
     // Getting the value and name of the input which triggered the change
     const { name, value } = event.target;
 
     //filter 
-    // const filteredEmployees = 
+    // const filteredEmployees = [{}];
+
+
+    //getting the filter down
+    console.log(employees)
+
+    //Question - why is my search term behind by one letter? *******
+    const searchTerm = this.state.search;
+    console.log(searchTerm)
+
+  
+    const filteredEmp = employees.filter(item => item.name.includes(searchTerm))
+    console.log(filteredEmp)
+
+
+    //Sort Order
+
+
+
+
 
     // Updating the input's state
     this.setState({
@@ -27,14 +50,18 @@ class App extends React.Component {
     });
   };
 
+ 
+
   handleFormSubmit = event => {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     event.preventDefault();
+    console.log("employees coming up")
+    console.log(employees)
 
-    // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
-    alert(`Hello ${this.state.firstName}`);
+    
+    // Clear `this.state.search`, clearing the inputs
     this.setState({
-      firstName: ""
+      search: ""
     });
   };
 
@@ -44,20 +71,24 @@ class App extends React.Component {
       <div>
         <Header></Header>
         <p>
-          Hello {this.state.firstName}
+          Hello {this.state.search}
         </p>
         <form className="form">
           <input
-            value={this.state.firstName}
-            name="firstName"
+            value={this.state.search}
+            name="search"
             onChange={this.handleInputChange}
             type="text"
-            placeholder="First Name"
+            placeholder="Name"
           />
          
           <button onClick={this.handleFormSubmit}>Submit</button>
         </form>
-        <EmployeeTable></EmployeeTable>
+        <EmployeeTable>
+          {/* <tableHead/> */}
+          {/* employees.map(users => () {}) */}
+          {/* <tableRow/> */}
+        </EmployeeTable>
       </div>
     );
   }
